8b69cb2b1e0cfb30cb4a7e0b785babb2b1d1c2ee
Muhammad-ipal <mipaldev@gmail.com>
2025-07-31 18:43:18 +0700

    feat: creating user (seeder)

---
+++ backend/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\UserController;
+
+Route::prefix('mobile')->group(function () {
+  require __DIR__ . '/mobile.php';
+});
+
+// Kalo fungsi auth sudah berfungsi pakaikan middleware ini
+// ->middleware('auth:sanctum')
+
+Route::prefix('web')->group(function () {
+  Route::get('/users', [UserController::class, 'index']);
+});
