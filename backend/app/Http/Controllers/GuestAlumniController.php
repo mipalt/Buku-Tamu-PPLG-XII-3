93f8206d0376cae1e4441a0596c83eb3e684b2bc
pannca <pancaprasetia@smkwikrama.sch.id>
2025-08-02 11:33:59 +0700

    up

--- backend/app/Http/Controllers/GuestAlumniController.php
+++ backend/app/Http/Controllers/GuestAlumniController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Alumni;
 
-class GuestAlumni extends Controller
+class GuestAlumniController extends Controller
 {
     /**
      * Display a listing of the resource.
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestAlumniController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/guest-alumni', [GuestAlumniController::class, 'index'])->name('guest-alumni.index');
