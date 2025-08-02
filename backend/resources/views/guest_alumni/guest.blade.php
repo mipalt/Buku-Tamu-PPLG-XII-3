93fdf271e9369ce84766bc5434ea5ff447436cfe
pannca <pancaprasetia@smkwikrama.sch.id>
2025-08-02 13:16:27 +0700

    up

--- backend/resources/views/guest_alumni/guest.blade.php
+++ backend/resources/views/guest_alumni/guest.blade.php
@@ -11,25 +11,26 @@
     <form action="{{ route('guest.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <label>Nama:</label><br>
-        <input type="text" name="name" required><br><br>
+        <input type="text" name="name"  value="{{ old('name') }}" required><br><br>
 
         <label>Tahun Lulus:</label><br>
-        <input type="number" name="graduation_year" required><br><br>
+        <input type="number" name="graduation_year" value="{{ old('graduation_year') }}" required><br><br>
 
         <label>Jurusan:</label><br>
         <select name="major" required>
             <option value="">-- Pilih Jurusan --</option>
             <option value="PPLG">PPLG</option>
+            <option value="PPLG">TJKT</option>
         </select><br><br>
 
         <label>Phone:</label><br>
-        <input type="number" name="phone" required><br><br>
+        <input type="number" name="phone" value="{{ old('phone') }}"  required><br><br>
 
         <label>Email:</label><br>
-        <input type="email" name="email"><br><br>
+        <input type="email" name="email" value="{{ old('email') }}" ><br><br>
 
         <label>Tujuan:</label><br>
-        <textarea name="purpose" required></textarea><br><br>
+        <textarea name="purpose" value="{{ old('purpose') }}"  required></textarea><br><br>
 
         <label>Tanda Tangan:</label><br>
         <input type="file" name="signature_path"><br><br>
