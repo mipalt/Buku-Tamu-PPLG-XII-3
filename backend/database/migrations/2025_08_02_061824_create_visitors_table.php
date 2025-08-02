e2af4450367dac4fc11e2fbd09f8ce1931e5ab1c
annasyasa <annasyasiti1@gmail.com>
2025-08-02 15:35:52 +0700

    guest visitor table migration n model

---
+++ backend/database/migrations/2025_08_02_061824_create_visitors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('guest_visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('institution', 100)->nullable();
+            $table->string('phone', 20);
+            $table->string('email', 100)->nullable();
+            $table->text('purpose', 100);
+            $table->string('signature_path', 255);
+            $table->timestamp('create_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('guest_visitors');
+    }
+};
