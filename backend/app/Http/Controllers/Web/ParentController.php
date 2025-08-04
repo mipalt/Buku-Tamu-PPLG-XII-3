d3bb56fdf002c1996289fd26baf09fae54cf82cb
Braincode Sinergi <braincodesinergi@gmail.com>
2025-08-04 21:10:11 +0700

    update

--- backend/app/Http/Controllers/Web/ParentController.php
+++ backend/app/Http/Controllers/Web/ParentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use  App\Models\Parents;
+use Illuminate\Support\Str;
+use App\Service\ImageUploadService;
 
 class ParentController extends Controller
 {
@@ -20,22 +22,31 @@ class ParentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-          $parent = Parents::create($request->validate([
-            'name' => 'required|string|max:100',
-            'student_name' => 'required|string|max:100',
-            'rayon' => 'required|string|max:10',
-            'address' => 'required|string|max:150',
-            'phone' => 'required|number|max:20',
-            'email' => 'nullable|email|unique:parents', 
-            'purpose' => 'required|text',
-            'signature_path' => 'required|string|max:150',
-        ]));
+  public function store(Request $request, ImageUploadService $imageUploadService)
+{
+    $data = $request->validate([
+        'name' => 'required',
+        'student_name' => 'required',
+        'rayon' => 'required',
+        'address' => 'required',
+        'phone' => 'required|string|max:20',
+        'email' => 'nullable|email|unique:parents',
+        'purpose' => 'required',
+        'signature_path' => 'required|file|mimes:jpg,jpeg,png|max:2048',
+    ]);
 
-        return response()->json($parent, 201);
+    if ($request->hasFile('signature_path')) {
+        $uploadedPath = $imageUploadService->upload($request->file('signature_path'), 'parent');
+        $data['signature_path'] = $uploadedPath;
     }
+
+    $parent = Parents::create($data);
+
+    return response()->json([
+        'message' => 'Data berhasil disimpan',
+        'data' => $parent
+    ], 201);
+}
 
     /**
      * Display the specified resource.
