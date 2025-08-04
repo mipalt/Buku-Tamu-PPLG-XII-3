0e5f08dcfee074ac18f7731db46a8a05d3a48d04
Muhammad-ipal <mipaldev@gmail.com>
2025-08-04 21:37:33 +0700

    feat: post user

--- backend/app/Http/Controllers/Web/ParentController.php
+++ backend/app/Http/Controllers/Web/ParentController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Services\ImageUploadService;
 use Illuminate\Http\Request;
-use  App\Models\Parents;
+use App\Models\Parents;
 use Illuminate\Support\Str;
-use App\Service\ImageUploadService;
 
 class ParentController extends Controller
 {
@@ -22,31 +22,31 @@ class ParentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-  public function store(Request $request, ImageUploadService $imageUploadService)
-{
-    $data = $request->validate([
-        'name' => 'required',
-        'student_name' => 'required',
-        'rayon' => 'required',
-        'address' => 'required',
-        'phone' => 'required|string|max:20',
-        'email' => 'nullable|email|unique:parents',
-        'purpose' => 'required',
-        'signature_path' => 'required|file|mimes:jpg,jpeg,png|max:2048',
-    ]);
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'student_name' => 'required',
+            'rayon' => 'required',
+            'address' => 'required',
+            'phone' => 'required|string|max:20',
+            'email' => 'nullable|email|unique:parents',
+            'purpose' => 'required',
+            'signature_path' => 'required|file|mimes:jpg,jpeg,png|max:2048',
+        ]);
 
-    if ($request->hasFile('signature_path')) {
-        $uploadedPath = $imageUploadService->upload($request->file('signature_path'), 'parent');
-        $data['signature_path'] = $uploadedPath;
+        if ($request->hasFile('signature_path')) {
+            $uploadedPath = ImageUploadService::upload($request->file('signature_path'), 'parent');
+            $data['signature_path'] = $uploadedPath;
+        }
+
+        $parent = Parents::create($data);
+
+        return response()->json([
+            'message' => 'Data berhasil disimpan',
+            'data' => $parent
+        ], 201);
     }
-
-    $parent = Parents::create($data);
-
-    return response()->json([
-        'message' => 'Data berhasil disimpan',
-        'data' => $parent
-    ], 201);
-}
 
     /**
      * Display the specified resource.
