224cb40e7fc521cdb63b3e8821d1ab65dda4deac
Muhammad-ipal <mipaldev@gmail.com>
2025-08-05 12:34:22 +0700

    feat: create visitor table

---
+++ backend/app/Models/Purpose.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Purpose extends Model
+{
+    protected $fillable = [
+        'name',
+        'visitor_id',
+        'guest_type'
+    ];
+
+    // public function visitor()
+    // {
+    //     return $this->morphTo(__FUNCTION__, 'guest_type', 'visitor_id');
+    // }
+
+    // di model
+    // public function purposes()
+    // {
+    //     return $this->morphMany(Purpose::class, 'visitor', 'guest_type', 'visitor_id');
+    // }
+
+}
